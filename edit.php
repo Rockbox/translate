Rockbox/translate

commit 81513daba744a4e9fb8ea6c95491bdc4faf899b5

Normalize most of the quotes
- single quotes for PHP strings without escape sequences
- double quotes for PHP strings with escape sequences
- double quotes for HTML attributes

This change is mechanical, fixing the remaining quotes requires changing the code flow, and it will be a follow-up patch.

Change-Id: I7b385eb2ebc4236e2497f43e9294751afecd2a80

edit.php
@@ -56,10 +56,10 @@ echo <<<END
 <input type="hidden" name="lang" value="$lang" />
 END;
 
-    $phrases = parselangfile(sprintf("scratch/%s.lang.update", $lang), $all);
-    $english = parselangfile(sprintf("scratch/%s.lang.update", 'english'), $all);
+    $phrases = parselangfile(sprintf('scratch/%s.lang.update', $lang), $all);
+    $english = parselangfile(sprintf('scratch/%s.lang.update', 'english'), $all);
     if ($phrases === false || $english === false) {
-        printf("<strong>The file %s.lang doesn't exist, or something else went terribly wrong</strong>", $lang);
+        printf('<strong>The file %s.lang doesn't exist, or something else went terribly wrong</strong>', $lang);
         return false;
     }
 
@@ -67,8 +67,8 @@ END;
     $inputdir = isset($languageinfo[$lang]['rtl']) && $languageinfo[$lang]['rtl'] === true ? " dir='rtl' " : '';
     foreach($phrases as $id => $phrase) {
         $foo = array();
-        if (sizeof($phrase['notes']) > 0 && trim(strtolower($phrase['phrase']['desc'])) != "deprecated") {
-            printf("<h3>%s</h3>", $phrase['phrase']['id']);
+        if (sizeof($phrase['notes']) > 0 && trim(strtolower($phrase['phrase']['desc'])) != 'deprecated') {
+            printf('<h3>%s</h3>', $phrase['phrase']['id']);
 
             if (isset($phrase['phrase']['desc']))
                 printf("Description: %s<br />\n", $phrase['phrase']['desc']);
@@ -76,7 +76,7 @@ END;
                 printf("User: %s<br />\n", $phrase['phrase']['user']);
 
             if (sizeof($phrase['notes']) > 0) {
-                print("<div class='note'>");
+                print('<div class="note">');
                 foreach($phrase['notes'] as $line) {
 		    if (preg_match('/The <(.+)> section for \'.+:(.*)\'.+/', $line, $matches)) {
 	                $foo["$matches[1]:$matches[2]"] = 1;
@@ -86,10 +86,10 @@ END;
 	            }
                     printf("%s<br />\n", htmlspecialchars($line));
                 }
-                print("</div>");
+                print('</div>');
             }
 
-            printf("<table><thead><tr><td>Target/feature</td><td>English string</td><td>%s translation</td><td>English voice</td><td>%s voice</td></tr></thead>", $languageinfo[$lang]['name'], $languageinfo[$lang]['name']);
+            printf('<table><thead><tr><td>Target/feature</td><td>English string</td><td>%s translation</td><td>English voice</td><td>%s voice</td></tr></thead>', $languageinfo[$lang]['name'], $languageinfo[$lang]['name']);
             foreach($phrase['source'] as $target => $string) {
                 // Figure out what to put in the translated string
                 if (isset($english[$id]['dest'][$target]) && ($english[$id]['dest'][$target] == '' || $english[$id]['dest'][$target] == 'none')) {
@@ -143,15 +143,15 @@ END;
                     $voice_readonly = '';
                 }
 
-                print("<tr>");
+                print('<tr>');
 		if (array_key_exists("dest:$target", $foo) ||
-		    array_key_exists("dest", $foo)) {
+		    array_key_exists('dest', $foo)) {
 			$bgcolor='class="poor"';
                 } else {
 			$bgcolor='';
 		}
 
-                printf("<td>%s</td><td>%s</td><td $bgcolor><input %s %s name='phrases[%s][dest][%s]' size='40' type='text' value='%s' %s /></td>",
+                printf('<td>%s</td><td>%s</td><td $bgcolor><input %s %s name="phrases[%s][dest][%s]" size="40" type="text" value="%s" %s /></td>',
                     htmlspecialchars($target),
                     htmlspecialchars($string),
                     $inputlang,
@@ -163,18 +163,18 @@ END;
                 );
 
 		if (array_key_exists("voice:$target", $foo) ||
-		    array_key_exists("voice", $foo)) {
-			$bgcolor='class="poor"';
+		    array_key_exists('voice', $foo)) {
+			$bgcolor='class='poor'';
                 } else {
 			$bgcolor='';
 		}
 
 
                 if (!isset($english[$id]['voice'][$target])) {
-                    print("<td colspan='2'></td>");
+                    print('<td colspan="2"></td>');
                 }
                 else {
-                    printf("<td>%s</td><td $bgcolor><input %s %s name='phrases[%s][voice][%s]' size='40' type='text' value='%s' %s /></td>",
+                    printf('<td>%s</td><td $bgcolor><input %s %s name="phrases[%s][voice][%s]" size="40" type="text" value="%s" %s /></td>',
                         htmlspecialchars(isset($english[$id]['voice'][$target]) ? $english[$id]['voice'][$target] : ''),
                         $inputlang,
                         $inputdir,
@@ -186,10 +186,10 @@ END;
                 }
                 print("</tr>\n");
             }
-            print("</table>");
+            print('</table>');
         }
-        elseif (trim(strtolower($phrase['phrase']['desc'])) == "deprecated") {
-            printf("<input type='hidden' name='phrases[%s][dest][*]' value='' /><input type='hidden' name='phrases[%s][dest][*]' value='' />",
+        elseif (trim(strtolower($phrase['phrase']['desc'])) == 'deprecated') {
+            printf('<input type="hidden" name="phrases[%s][dest][*]" value="" /><input type="hidden" name="phrases[%s][dest][*]" value="" />',
                 htmlspecialchars($id),
                 htmlspecialchars($id)
             );

common.php
@@ -91,10 +91,10 @@ function parselangfile($filename, $all = false) {
         $line = trim($line);
 
 	if ($all == true) {
-            $thisphrase['notes'][0] = "All fields are editable";
+            $thisphrase['notes'][0] = 'All fields are editable';
         }
         if (preg_match("/^### (.*)$/", $line, $matches)) {
-            if (strpos($matches[1], "The phrase is not used. Skipped") === false) {
+            if (strpos($matches[1], 'The phrase is not used. Skipped') === false) {
                 $thisphrase['notes'][] = $matches[1];
             }
         }

templater.class.php
@@ -26,11 +26,11 @@ class templater {
 
     public function __construct($smartydir) {
         /* Load and set up Smarty */
-        require_once(sprintf("%s/Smarty.class.php", $smartydir));
+        require_once(sprintf('%s/Smarty.class.php', $smartydir));
         $s = new smarty();
-        $s->setTemplateDir("templates");
-        $s->setCompileDir("templates/compiled");
-        $s->setCacheDir("templates/cache");
+        $s->setTemplateDir('templates');
+        $s->setCompileDir('templates/compiled');
+        $s->setCacheDir('templates/cache');
 //        $s->caching = false;
 //        $s->debugging = false;
 //        $s->security = true;
@@ -47,34 +47,34 @@ class templater {
         $one_hour = 60*60;
         $one_day = 24*$one_hour;
         if ($seconds > 60*28 && $seconds < 60*32) {
-            return "half an hour ago";
+            return 'half an hour ago';
         }
         elseif ($seconds < $one_hour) {
             $minutes = floor($seconds/60);
-            return sprintf("%d minute%s ago", $minutes, qs($minutes));
+            return sprintf('%d minute%s ago', $minutes, qs($minutes));
         }
         elseif ($seconds < $one_day) {
             $hours = floor($seconds/$one_hour);
-            return sprintf("%d hour%s ago", $hours, qs($hours));
+            return sprintf('%d hour%s ago', $hours, qs($hours));
         }
         elseif ($seconds < 2*$one_day) {
-            return "Yesterday";
+            return 'Yesterday';
         }
         elseif ($seconds < 7*$one_day) {
             $days = floor($seconds/$one_day);
-            return sprintf("%d day%s ago", $days, qs($days));
+            return sprintf('%d day%s ago', $days, qs($days));
         }
         elseif ($seconds < 2*30*$one_day) {
             $weeks = floor($seconds/(7*$one_day));
-            return sprintf("%d week%s ago", $weeks, qs($weeks));
+            return sprintf('%d week%s ago', $weeks, qs($weeks));
         }
         elseif ($seconds < 365*$one_day) {
             $months = floor($seconds/(30*$one_day));
-            return sprintf("%d month%s ago", $months, qs($months));
+            return sprintf('%d month%s ago', $months, qs($months));
         }
         else {
             $years = floor($seconds/(365*$one_day));
-            return sprintf("%d year%s ago", $years, qs($years));
+            return sprintf('%d year%s ago', $years, qs($years));
         }
     }
 

templates/footer.tpl
@@ -3,7 +3,7 @@
   <img src="//www.rockbox.org/rockbox100.png" border="0" width="100" height="32" alt="www.rockbox.org" title="Rockbox - Open Source Jukebox Firmware" />
 </a>
 <small>
-Last updated {$updated|date_format:"%c"}. Flags copyright Wikimedia contributors.
+Last updated {$updated|date_format:'%c'}. Flags copyright Wikimedia contributors.
 </small>
 
 </body>

templates/frontpage.tpl
@@ -1,4 +1,4 @@
-{include file="header.tpl" title=$title rss="rss.php"}
+{include file='header.tpl' title=$title rss='rss.php'}
 
 <h1>Rockbox translations</h1>
 
@@ -21,9 +21,9 @@ possibly slightly safer way.
 <ul>
 {foreach from=$summary key=k item=v}
     <li>{$v} {$k} translation{if $v!=1}s{/if}
-    {if $k=="good"}(&gt;85% translated){/if}
-    {if $k=="normal"}(&gt;50% translated){/if}
-    {if $k=="bad"}(&lt;50% translated){/if}
+    {if $k=='good'}(&gt;85% translated){/if}
+    {if $k=='normal'}(&gt;50% translated){/if}
+    {if $k=='bad'}(&lt;50% translated){/if}
     </li>
 {/foreach}
 </ul>
@@ -32,7 +32,7 @@ possibly slightly safer way.
 <table>
     <thead>
     <tr>
-        <td colspan='3'>Language</td>
+        <td colspan="3">Language</td>
         <td>Voiced</td>
         <td>Last update</td>
         <td>Progress</td>
@@ -73,7 +73,7 @@ possibly slightly safer way.
         {if $language.percentage == 100 && $language.desc == 0 && $language.source == 0 && $language.dest == 0 && $language.voice == 0 && ($language.voicedup + $language.destdup) == 0 }
             {$language.name}
         {else}
-            <a href='edit.php?lang={$langfile}'>{$language.name}</a>
+            <a href="edit.php?lang={$langfile}">{$language.name}</a>
         {/if}
         </td>
         <td>
@@ -82,13 +82,13 @@ possibly slightly safer way.
         {/if}
         </td>
         <td>
-            <a href='//git.rockbox.org/cgit/rockbox.git/commit/?id={$language.last_update_rev}' title='{$language.last_update|date_format:"%c"}'>
+            <a href="//git.rockbox.org/cgit/rockbox.git/commit/?id={$language.last_update_rev}" title="{$language.last_update|date_format:'%c'}">
                 {$language.last_update|simple_timesince}
             </a>
         </td>
         <td>
-            <div class="progress-wrapper" title="{$language.percentage|string_format:"%.2f"}%">
-                <div class="progress-completed" style="width: {$language.percentage|string_format:"%.2f"}%"></div>
+            <div class="progress-wrapper" title="{$language.percentage|string_format:'%.2f'}%">
+                <div class="progress-completed" style="width: {$language.percentage|string_format:'%.2f'}%"></div>
             </div>
         </td>
         </td>
@@ -102,4 +102,4 @@ possibly slightly safer way.
     {/foreach}
 </table>
 
-{include file="footer.tpl"}
+{include file='footer.tpl'}

update.php
@@ -26,7 +26,7 @@ function update_langs() {
     $fp = fopen(VERSIONS, 'w');
     foreach(glob('rockbox/apps/lang/*.lang') as $lang) {
         $gitstr = shell_exec(sprintf("cd rockbox && git log --pretty=%%H -1 %s",
-                "apps/lang/" . basename($lang)));
+                'apps/lang/' . basename($lang)));
         $line = sprintf("%s:%s\n", basename($lang, '.lang'), trim($gitstr));
         fwrite($fp, $line);
     }
@@ -37,7 +37,7 @@ function update_langs() {
 function genstats() {
     $langs = array();
     foreach(file(VERSIONS) as $line) {
-        list($lang, $version) = explode(":", trim($line));
+        list($lang, $version) = explode(':', trim($line));
         $langs[$lang] = $version;
     }
 
@@ -45,11 +45,11 @@ function genstats() {
     foreach($langs as $lang => $rev) {
         $foo = array();
         $err = 0;
-        $cmd = sprintf("%s -s rockbox/tools/updatelang rockbox/apps/lang/english.lang rockbox/apps/lang/%s.lang -", PERL, $lang);
+        $cmd = sprintf('%s -s rockbox/tools/updatelang rockbox/apps/lang/english.lang rockbox/apps/lang/%s.lang -', PERL, $lang);
         $output = shell_exec($cmd);
 //        print("$ $cmd\n");
 //        printf("%s\n", $output);
-        file_put_contents(sprintf("scratch/%s.lang.update", $lang), $output);
+        file_put_contents(sprintf('scratch/%s.lang.update', $lang), $output);
         list($lastrev, $lastupdate) = getlastupdated($lang);
             $stat = array('name' => $lang, 'total' => 0, 'error' => 0, 'missing' => 0, 'desc' => 0, 'source' => 0, 'dest' => 0, 'destdup' => 0, 'voice' => 0, 'voicedup' => 0, 'last_update' => $lastupdate, 'last_update_rev' => $lastrev);
         foreach(explode("\n", $output) as $line) {
@@ -119,7 +119,7 @@ function genstats() {
 //            $info['percentage']--; // XXX for testing only.
 	    foreach($maintainers[$lang] as $id => $email) {
               if ($info['percentage'] < $oldinfo['percentage']) {
-	        $headers = sprintf("From: %s", OUTBOUND_EMAIL);
+	        $headers = sprintf('From: %s', OUTBOUND_EMAIL);
 		$subject = sprintf("Rockbox '%s' translation needs updating\n", $lang);
 		$msg = sprintf("
 
@@ -157,19 +157,19 @@ function getlastupdated($lang) {
     $retries = 0;
     while ($retries < 5) {
         try {
-            $gitstr = shell_exec(sprintf("cd rockbox && git log --pretty=%%H,%%ct -200 apps/lang/%s.lang", $lang));
+            $gitstr = shell_exec(sprintf('cd rockbox && git log --pretty=%%H,%%ct -200 apps/lang/%s.lang', $lang));
             $line = sprintf("%s:%s\n", basename($lang, '.lang'), $gitstr);
             $retries = 100;
         }
         catch (Exception $e) {
             $retries++;
             printf("Warning: Caught exception: %s (trying again)<br />\n", $e->getMessage());
-            if ($retries > 5) die("Cannot succeed :(");
+            if ($retries > 5) die('Cannot succeed :(');
         }
     }
     $ignorehash = explode("\n", file_get_contents('ignoredhash.list'));
     foreach(preg_split('(\n\r|\r\n|\r|\n)', $gitstr) as $logentry) {
-        list($rev, $date) = explode(",", trim($logentry));
+        list($rev, $date) = explode(',', trim($logentry));
         if(!in_array($rev, $ignorehash)) {
             return array($rev, $date);
         }
@@ -179,7 +179,7 @@ function getlastupdated($lang) {
 
 function update_flags() {
     $languageinfo = languageinfo();
-    $path = "e/e6";
+    $path = 'e/e6';
 
     if (!file_exists('flags')) {
         mkdir('flags');

whichfont.php
@@ -18,10 +18,10 @@
  *
  **************************************************************************/
 
-header("Content-type: text/html; charset=UTF-8");
+header('Content-type: text/html; charset=UTF-8');
 require_once('common.php');
 /* Set internal character encoding to UTF-8 */
-mb_internal_encoding("UTF-8");
+mb_internal_encoding('UTF-8');
 ?>
 <!DOCTYPE html>
 <html>
@@ -108,7 +108,7 @@ foreach($fontstats as $lang => $stats) {
     }
     printf("  <tr>\n   <td class='lang' title='%.2f%% complete'><img src='flags/%d/%s.png' /> %s</td>\n", $lang_stats['langstats'][$lang]['percentage'], SMALL_FLAGSIZE, urlencode($langs[$lang]['flag']), $langs[$lang]['name']);
     foreach($stats as $font => $coverage) {
-        $hover = "";
+        $hover = '';
         if (isset($fontstats["missing|$lang"]) && isset($fontstats["missing|$lang"][$font])) {
             $hover = $fontstats["missing|$lang"][$font];
         }

commit e5fbc53b06e83cdf4929a0298a64254272aa6334

fix regression in edit.php
Introduced in 81513daba744a4e9fb8ea6c95491bdc4faf899b5

edit.php
@@ -59,7 +59,7 @@ END;
     $phrases = parselangfile(sprintf('scratch/%s.lang.update', $lang), $all);
     $english = parselangfile(sprintf('scratch/%s.lang.update', 'english'), $all);
     if ($phrases === false || $english === false) {
-        printf('<strong>The file %s.lang doesn't exist, or something else went terribly wrong</strong>', $lang);
+        printf('<strong>The file %s.lang does not exist, or something else went terribly wrong</strong>', $lang);
         return false;
     }
 
@@ -164,7 +164,7 @@ END;
 
 		if (array_key_exists("voice:$target", $foo) ||
 		    array_key_exists('voice', $foo)) {
-			$bgcolor='class='poor'';
+			$bgcolor='class="poor"';
                 } else {
 			$bgcolor='';
 		}